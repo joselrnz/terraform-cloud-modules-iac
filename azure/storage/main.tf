475ad8135955b75788af09518bc7548904fe1211
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-01 14:11:23 -0500

    update

--- azure/storage/main.tf
+++ azure/storage/main.tf
@@ -15,28 +15,22 @@ resource "azurerm_storage_account" "this" {
   
   tags = var.tags
 
-  dynamic "blob_properties" {
-    for_each = var.enable_versioning || var.enable_delete_retention || var.enable_container_delete_retention ? [1] : []
-    content {
-      dynamic "versioning_enabled" {
-        for_each = var.enable_versioning ? [1] : []
-        content {
-          enabled = true
-        }
-      }
+  blob_properties {
+    # versioning_enabled is a boolean property, not a block
+    versioning_enabled = var.enable_versioning
 
-      dynamic "delete_retention_policy" {
-        for_each = var.enable_delete_retention ? [1] : []
-        content {
-          days = var.delete_retention_days
-        }
+    # These remain as dynamic blocks because they have their own properties
+    dynamic "delete_retention_policy" {
+      for_each = var.enable_delete_retention ? [1] : []
+      content {
+        days = var.delete_retention_days
       }
+    }
 
-      dynamic "container_delete_retention_policy" {
-        for_each = var.enable_container_delete_retention ? [1] : []
-        content {
-          days = var.container_delete_retention_days
-        }
+    dynamic "container_delete_retention_policy" {
+      for_each = var.enable_container_delete_retention ? [1] : []
+      content {
+        days = var.container_delete_retention_days
       }
     }
   }
@@ -48,3 +42,4 @@ resource "azurerm_storage_container" "this" {
   storage_account_name  = azurerm_storage_account.this.name
   container_access_type = each.value.access_type
 }
+
