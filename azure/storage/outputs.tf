e3d3c13378356641c76403d6ea50a4ffaacf1fad
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-08 20:33:14 -0500

    update

--- azure/storage/outputs.tf
+++ azure/storage/outputs.tf
@@ -35,10 +35,6 @@ output "name" {
   value = azurerm_storage_account.this.name
 }
 
-output "primary_blob_endpoint" {
-  value = azurerm_storage_account.this.primary_blob_endpoint
-}
-
 output "id" {
   value = azurerm_storage_account.this.id
 }
