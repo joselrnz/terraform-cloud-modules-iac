6f4c4a142b00480c18aef60f909d54d3c58cfb4e
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-13 18:40:08 -0500

    update

--- azure/storage/main.tf
+++ azure/storage/main.tf
@@ -36,10 +36,10 @@ resource "azurerm_storage_account" "this" {
   }
 }
 
-# resource "azurerm_storage_container" "this" {
-#   for_each              = { for container in var.containers : container.name => container }
-#   name                  = each.key
-#   storage_account_name  = azurerm_storage_account.this.name
-#   container_access_type = each.value.access_type
-# }
+resource "azurerm_storage_container" "this" {
+  for_each              = { for container in var.containers : container.name => container }
+  name                  = each.key
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = each.value.access_type
+}
 
