82f5fb76082d96faed852086d03ed001c8607a41
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-18 15:57:09 -0500

    update tf scripts

--- azure/rbac/variables.tf
+++ azure/rbac/variables.tf
@@ -1,18 +1,3 @@
-variable "scope" {
-  description = "The resource ID where the role assignment applies (e.g., a storage account)"
-  type        = string
-}
-
-variable "role_definition_name" {
-  description = "The role to assign (e.g., 'Reader', 'Storage Blob Data Contributor')"
-  type        = string
-}
-
-variable "principal_id" {
-  description = "The object ID of the user, group, or service principal"
-  type        = string
-}
-
 variable "role_assignments" {
   description = "List of role assignments (principal_id + role + scope)"
   type = list(object({
