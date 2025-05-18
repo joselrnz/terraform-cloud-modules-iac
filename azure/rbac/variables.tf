9395e74fbb425351dcabbe49bece455a955dddb8
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-18 15:45:12 -0500

    update tf scripts

--- azure/rbac/variables.tf
+++ azure/rbac/variables.tf
@@ -12,3 +12,20 @@ variable "principal_id" {
   description = "The object ID of the user, group, or service principal"
   type        = string
 }
+
+variable "role_assignments" {
+  description = <<EOT
+List of IAM role assignments:
+- principal_id (required)
+- scope (required)
+- role_definition_name (optional)
+- role_definition_id (optional)
+Only one of role_definition_name or role_definition_id should be provided.
+EOT
+  type = list(object({
+    principal_id         = string
+    scope                = string
+    role_definition_name = optional(string)
+    role_definition_id   = optional(string)
+  }))
+}
