3c78b1fa13cddd5a78c725c06c9037f8c5b3a8fe
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-18 16:00:03 -0500

    update tf scripts

--- azure/rbac/outputs.tf
+++ azure/rbac/outputs.tf
@@ -1,4 +1,3 @@
-output "role_assignment_id" {
-  description = "The ID of the role assignment"
-  value       = azurerm_role_assignment.this.id
+output "role_assignment_ids" {
+  value = [for r in azurerm_role_assignment.dynamic : r.id]
 }
