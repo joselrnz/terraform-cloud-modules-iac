239ac9e167f2e7edfdbff0a38e3209f012001412
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-18 15:37:59 -0500

    update tf scripts

--- azure/rbac/main.tf
+++ azure/rbac/main.tf
@@ -3,3 +3,20 @@ resource "azurerm_role_assignment" "this" {
   role_definition_name = var.role_definition_name
   principal_id         = var.principal_id
 }
+
+
+
+resource "azurerm_role_assignment" "dynamic" {
+  for_each = {
+    for assignment in var.role_assignments :
+    "${assignment.principal_id}-${assignment.role_definition_name != null ? assignment.role_definition_name : assignment.role_definition_id}-${assignment.scope}" => assignment
+  }
+
+  principal_id = each.value.principal_id
+  scope        = each.value.scope
+
+  role_definition_name = lookup(each.value, "role_definition_name", null)
+  role_definition_id   = lookup(each.value, "role_definition_id", null)
+
+  depends_on = [each.value.scope]
+}
