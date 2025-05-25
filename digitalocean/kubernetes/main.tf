a224cd125343c0380fd24804a8544c35e3d9af17
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-24 21:29:45 -0500

    update tf files.

--- digitalocean/kubernetes/main.tf
+++ digitalocean/kubernetes/main.tf
@@ -1,32 +1,67 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = ">= 2.24"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     digitalocean = {
+#       source  = "digitalocean/digitalocean"
+#       version = ">= 2.24"
+#     }
+#   }
+# }
 
-
-resource "digitalocean_kubernetes_cluster" "this" {
-  name    = var.cluster_name
-  region  = var.region
-  version = var.kubernetes_version
-
+ Main Kubernetes cluster resource
+resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
+  name     = var.cluster_name
+  region   = var.region
+  version  = var.k8s_version
   vpc_uuid = var.vpc_id
 
+  # High availability control plane (optional but recommended for production)
+  ha = var.enable_ha
+
+  # Auto-upgrade settings
+  auto_upgrade = var.auto_upgrade
+  
+  # Surge upgrade settings
+  surge_upgrade = var.surge_upgrade
+
+  # Main node pool
   node_pool {
-    name       = "default-pool"
+    name       = var.pool_name
     size       = var.node_size
+    node_count = var.node_count
+    
+    # Auto-scaling configuration
+    auto_scale = var.enable_autoscale
     min_nodes  = var.min_nodes
     max_nodes  = var.max_nodes
-    auto_scale = true
+    
+    # Labels and taints
+    labels = var.node_labels
+    tags   = var.node_tags
   }
 
+  # Maintenance window
   maintenance_policy {
-    start_time = "04:00"
-    day        = "sunday"
+    start_time = var.maintenance_start_time
+    day        = var.maintenance_day
   }
 
-  tags = ["terraform", var.cluster_name]
+  # Cluster tags
+  tags = var.cluster_tags
+}
+
+# Additional node pool (optional)
+resource "digitalocean_kubernetes_node_pool" "additional_pool" {
+  count = var.create_additional_pool ? 1 : 0
+  
+  cluster_id = digitalocean_kubernetes_cluster.k8s_cluster.id
+  name       = var.additional_pool_name
+  size       = var.additional_pool_size
+  node_count = var.additional_pool_count
+  
+  auto_scale = var.additional_pool_autoscale
+  min_nodes  = var.additional_pool_min_nodes
+  max_nodes  = var.additional_pool_max_nodes
+  
+  labels = var.additional_pool_labels
+  tags   = var.additional_pool_tags
 }
--- digitalocean/kubernetes/variables.tf
+++ digitalocean/kubernetes/variables.tf
@@ -1,17 +1,171 @@
-variable "vpc_id" {}
-variable "region" {}
-variable "cluster_name" {}
-variable "kubernetes_version" {}
-variable "high_availability" {}
-variable "node_size" {}
-variable "min_nodes" {}
-variable "max_nodes" {}
-variable "spaces_bucket_name" {}
-variable "spaces_access_key" {}
-variable "spaces_secret_key" {}
-variable "application_namespaces" {
-  type = list(string)
+
+
+
+# Basic cluster configuration
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
+  type        = string
+  default     = "my-k8s-cluster"
 }
-variable "enable_ssl" {}
-variable "domain_name" {}
-variable "admin_email" {}
+
+variable "region" {
+  description = "DigitalOcean region"
+  type        = string
+  default     = "nyc1"
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.32.2-do.1"
+}
+
+variable "vpc_id" {
+  description = "VPC UUID"
+  type        = string
+  default     = null
+}
+
+# High availability and upgrade settings
+variable "enable_ha" {
+  description = "Enable high availability control plane"
+  type        = bool
+  default     = false
+}
+
+variable "auto_upgrade" {
+  description = "Enable auto-upgrade for the cluster"
+  type        = bool
+  default     = false
+}
+
+variable "surge_upgrade" {
+  description = "Enable surge upgrade"
+  type        = bool
+  default     = false
+}
+
+# Main node pool configuration
+variable "pool_name" {
+  description = "Name of the main node pool"
+  type        = string
+  default     = "main-pool"
+}
+
+variable "node_size" {
+  description = "Size of the nodes (droplet size)"
+  type        = string
+  default     = "s-2vcpu-2gb"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the main pool"
+  type        = number
+  default     = 2
+}
+
+# Auto-scaling configuration
+variable "enable_autoscale" {
+  description = "Enable auto-scaling for the main node pool"
+  type        = bool
+  default     = false
+}
+
+variable "min_nodes" {
+  description = "Minimum number of nodes when auto-scaling is enabled"
+  type        = number
+  default     = 1
+}
+
+variable "max_nodes" {
+  description = "Maximum number of nodes when auto-scaling is enabled"
+  type        = number
+  default     = 5
+}
+
+# Node labeling and tagging
+variable "node_labels" {
+  description = "Labels to apply to nodes"
+  type        = map(string)
+  default     = {}
+}
+
+variable "node_tags" {
+  description = "Tags to apply to nodes"
+  type        = list(string)
+  default     = []
+}
+
+variable "cluster_tags" {
+  description = "Tags to apply to the cluster"
+  type        = list(string)
+  default     = []
+}
+
+# Maintenance window
+variable "maintenance_start_time" {
+  description = "Start time for maintenance window (HH:MM format)"
+  type        = string
+  default     = "04:00"
+}
+
+variable "maintenance_day" {
+  description = "Day of the week for maintenance (monday, tuesday, etc.)"
+  type        = string
+  default     = "sunday"
+}
+
+# Additional node pool configuration
+variable "create_additional_pool" {
+  description = "Whether to create an additional node pool"
+  type        = bool
+  default     = false
+}
+
+variable "additional_pool_name" {
+  description = "Name of the additional node pool"
+  type        = string
+  default     = "additional-pool"
+}
+
+variable "additional_pool_size" {
+  description = "Size of nodes in the additional pool"
+  type        = string
+  default     = "s-4vcpu-8gb"
+}
+
+variable "additional_pool_count" {
+  description = "Number of nodes in the additional pool"
+  type        = number
+  default     = 1
+}
+
+variable "additional_pool_autoscale" {
+  description = "Enable auto-scaling for additional pool"
+  type        = bool
+  default     = false
+}
+
+variable "additional_pool_min_nodes" {
+  description = "Minimum nodes for additional pool auto-scaling"
+  type        = number
+  default     = 1
+}
+
+variable "additional_pool_max_nodes" {
+  description = "Maximum nodes for additional pool auto-scaling"
+  type        = number
+  default     = 3
+}
+
+variable "additional_pool_labels" {
+  description = "Labels for additional pool nodes"
+  type        = map(string)
+  default     = {}
+}
+
+variable "additional_pool_tags" {
+  description = "Tags for additional pool nodes"
+  type        = list(string)
+  default     = []
+}
